bc765845a6c2398c749f65f95fcbfe94e3964141
MichaelChandra0 <michaelchandra700@gmail.com>
2025-04-06 22:05:32 +0700

    update

--- script.js
+++ script.js
@@ -1,19 +1,26 @@
-document.addEventListener('DOMContentLoaded', () => {
+document.addEventListener("DOMContentLoaded", () => {
+  document.querySelector("button").addEventListener("click", () => {
+    const requestv4 = new XMLHttpRequest();
+    const requestv6 = new XMLHttpRequest();
+    // load IPv4
+    requestv4.onload = function () {
+      const data = JSON.parse(this.responseText);
+      const ip = data.ip;
+      document.getElementById("headingv4").innerHTML = "IPv4 : ";
+      document.getElementById("ipv4").innerHTML = ip;
+    };
 
-    document.querySelector("button").addEventListener("click", () => {
+    // load IPv6
+    requestv6.onload = function () {
+      const data = JSON.parse(this.responseText);
+      const ip = data.ip;
+      document.getElementById("headingv6").innerHTML = "IPv6 : ";
+      document.getElementById("ipv6").innerHTML = ip;
+    };
 
-        const request = new XMLHttpRequest();
-        request.onload = function(){
-            const data = JSON.parse(this.responseText);
-            const ip = data.ip;
-
-            document.getElementById("ip").innerHTML = ip
-
-
-        }
-
-        request.open("GET", "https://api.ipify.org/?format=json")
-        request.send();
-        
-    })
-})
+    requestv4.open("GET", "https://api.ipify.org?format=json");
+    requestv4.send();
+    requestv6.open("GET", "https://api6.ipify.org?format=json");
+    requestv6.send();
+  });
+});
